e32b9c1ce0329eb99b23bc8cbd6718f53cdfb569
Dmitry <burmitr@yandex.ru>
2022-08-04 23:51:09 +0500

    Sortirovka viborom

---
+++ Sortirovka_viborom/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+[6, 15, 2, 9, -3]
+MIN = 6
+6 < 15
+6 > 2
+MIN = 2
+2 < 9
+2 > -3
+MIN = -3
+[-3, 6, 15, 2, 9]
+[6, 15, 2, 9]
+MIN = 6
+6 < 15
+6 > 2
+MIN = 2
+2 < 9
+[-3, 2, 6, 15, 9]
+MIN = 6
+6 < 15
+6 < 9
+[-3, 2, 6, 15, 9]
+MIN = 15
+15 > 9
+[-3, 2, 6, 9, 15]
+*/
+
+Console.WriteLine("Введите кол-во элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+// Заполнение массива
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine();
+Console.WriteLine("Начальный массив: [" + string.Join(", ", array) + "]");
+
+// Сортировка
+for (int i = 0; i < n - 1; i++)
+{
+    int minIndex = i;
+    for (int j = i + 1; j < n; j++)
+    {
+        if (array[j] < array[minIndex])
+            minIndex = j;
+    }
+    int temp;
+    temp = array[minIndex];
+    array[minIndex]= array[i];
+    array[i] = temp;
+}
+Console.WriteLine("Конечный массив: [" + string.Join(", ", array) + "]");
